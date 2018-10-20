83e73808219efc3f146d98fe89f9c402604a5fea
PIneaPplez13 <pineappleslabs@gmail.com>
2018-10-20 03:55:53 -0400

    Google cloud vision api implementation but we are missing the api key

--- app/src/main/java/com/starkmedia/starkmedia/CameraPreview.java
+++ app/src/main/java/com/starkmedia/starkmedia/CameraPreview.java
@@ -1,15 +1,82 @@
 package com.starkmedia.starkmedia;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import android.content.Context;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import com.google.protobuf.ByteString;
+
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobListOption;
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.vision.v1.AnnotateFileResponse;
+import com.google.cloud.vision.v1.AnnotateFileResponse.Builder;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.AsyncAnnotateFileRequest;
+import com.google.cloud.vision.v1.AsyncAnnotateFileResponse;
+import com.google.cloud.vision.v1.AsyncBatchAnnotateFilesResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Block;
+import com.google.cloud.vision.v1.ColorInfo;
+import com.google.cloud.vision.v1.CropHint;
+import com.google.cloud.vision.v1.CropHintsAnnotation;
+import com.google.cloud.vision.v1.DominantColorsAnnotation;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.FaceAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.GcsDestination;
+import com.google.cloud.vision.v1.GcsSource;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageContext;
+import com.google.cloud.vision.v1.ImageSource;
+import com.google.cloud.vision.v1.InputConfig;
+import com.google.cloud.vision.v1.LocalizedObjectAnnotation;
+import com.google.cloud.vision.v1.LocationInfo;
+import com.google.cloud.vision.v1.OperationMetadata;
+import com.google.cloud.vision.v1.OutputConfig;
+import com.google.cloud.vision.v1.Page;
+import com.google.cloud.vision.v1.Paragraph;
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
+import com.google.cloud.vision.v1.Symbol;
+import com.google.cloud.vision.v1.TextAnnotation;
+import com.google.cloud.vision.v1.WebDetection;
+import com.google.cloud.vision.v1.WebDetection.WebEntity;
+import com.google.cloud.vision.v1.WebDetection.WebImage;
+import com.google.cloud.vision.v1.WebDetection.WebLabel;
+import com.google.cloud.vision.v1.WebDetection.WebPage;
+import com.google.cloud.vision.v1.WebDetectionParams;
+import com.google.cloud.vision.v1.Word;
+import java.lang.System.*;
+
+import com.google.protobuf.util.JsonFormat;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder mHolder;
     private Camera mCamera;
+    private int width;
+    private int height;
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
@@ -43,12 +110,11 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         } catch (Exception e) {
             // ignore: tried to stop a non-existent preview
         }
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-        // start preview with new settings
+
         setCamera(camera);
         try {
             mCamera.setPreviewDisplay(mHolder);
+            mCamera.setPreviewCallback(previewCallback);
             mCamera.startPreview();
         } catch (Exception e) {
             Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
@@ -56,9 +122,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
         refreshCamera(mCamera);
+        width = w;
+        height = h;
     }
 
     public void setCamera(Camera camera) {
@@ -68,7 +134,63 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // TODO Auto-generated method stub
         // mCamera.release();
     }
+
+    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+
+            //create a string  from the byte array
+            String imageBytes = data.toString();
+            YuvImage im = new YuvImage(data, ImageFormat.NV21, width, height, null);
+            int quality = 90;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Rect rect = new Rect(0, 0, width, height);
+            im.compressToJpeg(rect, quality, out);
+            ByteString contents = null;
+            try {
+                contents = ByteString.readFrom(new ByteArrayInputStream(out.toByteArray()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                // Instantiates a client
+                try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+                    // Build the image
+                    Image image = Image.newBuilder().setContent(contents).build();
+
+                    // Create the request with the image and the specified feature: web detection
+                    AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                            .addFeatures(Feature.newBuilder().setType(Type.WEB_DETECTION))
+                            .setImage(image)
+                            .build();
+
+                    // Perform the request
+                    BatchAnnotateImagesResponse response = client.batchAnnotateImages(Arrays.asList(request));
+
+                    // Display the results
+                    List<AnnotateImageResponse> responses = response.getResponsesList();
+                    for (AnnotateImageResponse res : responses) {
+                        if (res.hasError()) {
+                            System.out.println("yikes");
+                        } else {
+                            WebDetection annotation = res.getWebDetection();
+                            String description = annotation.getWebEntities(0).getDescription();
+                            ((MainActivity) getContext()).setImageBytes(description);
+                            System.out.println(description);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            //use the data
+            camera.startPreview();
+        }
+    };
 }
--- app/src/main/java/com/starkmedia/starkmedia/MainActivity.java
+++ app/src/main/java/com/starkmedia/starkmedia/MainActivity.java
@@ -1,18 +1,23 @@
 package com.starkmedia.starkmedia;
 
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Camera camera;
-    private CameraPreview mPreview;
+    private CameraPreview cameraPreview;
     private Context myContext;
-    private LinearLayout cameraPreview;
+    private LinearLayout cameraPreviewContainer;
+    private TextView byteString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +26,73 @@ public class MainActivity extends AppCompatActivity {
 
         myContext = this;
 
+        synchronizedOpenCamera();
+
+        camera.setDisplayOrientation(90);
+
+        //set camera to continually auto-focus
+        Camera.Parameters params = camera.getParameters();
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        camera.setParameters(params);
+
+        byteString = findViewById(R.id.byteString);
+        cameraPreviewContainer = findViewById(R.id.cPreview);
+        cameraPreview = new CameraPreview(myContext, camera);
+        cameraPreviewContainer.addView(cameraPreview);
+
+
+        camera.startPreview();
+    }
+
+    private void safeOpenCamera() {
         try {
             releaseCamera();
             camera = Camera.open();
-            camera.setDisplayOrientation(90);
-
-            //set camera to continually auto-focus
-            Camera.Parameters params = camera.getParameters();
-            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-            camera.setParameters(params);
-
         } catch (Exception e) {
             Log.e(getString(R.string.app_name), "failed to open Camera");
             e.printStackTrace();
         }
+    }
 
-        cameraPreview = findViewById(R.id.cPreview);
-        mPreview = new CameraPreview(myContext, camera);
-        cameraPreview.addView(mPreview);
+    private void synchronizedOpenCamera() {
+        if (cameraHandlerThread == null) {
+            cameraHandlerThread = new CameraHandlerThread();
+        }
 
-        camera.startPreview();
+        synchronized (cameraHandlerThread) {
+            cameraHandlerThread.openCamera();
+        }
+    }
+
+    private CameraHandlerThread cameraHandlerThread = null;
+    private class CameraHandlerThread extends HandlerThread {
+        Handler mHandler;
+
+        CameraHandlerThread() {
+            super("CameraHandlerThread");
+            start();
+            mHandler = new Handler(getLooper());
+        }
+
+        synchronized void notifyCameraOpened() {
+            notify();
+        }
+
+        void openCamera() {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    safeOpenCamera();
+                    notifyCameraOpened();
+                }
+            });
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+                Log.e(getString(R.string.app_name), "wait was interrupted");
+            }
+        }
     }
 
     public void onResume() {
@@ -49,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         if(camera == null) {
             camera = Camera.open();
             camera.setDisplayOrientation(90);
-            mPreview.refreshCamera(camera);
+            cameraPreview.refreshCamera(camera);
             Log.d("nu", "null");
         }else {
             Log.d("nu","no null");
@@ -73,5 +125,14 @@ public class MainActivity extends AppCompatActivity {
             camera.release();
             camera = null;
         }
+    }
+
+    public void setImageBytes(final String data) {
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                byteString.setText((data));
+            }
+        });
     }
 }
