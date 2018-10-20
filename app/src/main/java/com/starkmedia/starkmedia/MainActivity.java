7316a57a3736d822fc7d55efd051c399300240e8
PIneaPplez13 <pineappleslabs@gmail.com>
2018-10-19 23:02:47 -0400

    Broken Camera View

--- app/src/main/java/com/starkmedia/starkmedia/MainActivity.java
+++ app/src/main/java/com/starkmedia/starkmedia/MainActivity.java
@@ -1,13 +1,63 @@
 package com.starkmedia.starkmedia;
 
+import android.content.Context;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Context myContext;
+    private LinearLayout cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myContext = this;
+
+        mCamera =  Camera.open();
+        mCamera.setDisplayOrientation(90);
+        cameraPreview = (LinearLayout) findViewById(R.id.cPreview);
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
+        mCamera.startPreview();
+    }
+
+    public void onResume() {
+
+        super.onResume();
+        if(mCamera == null) {
+            mCamera = Camera.open();
+            mCamera.setDisplayOrientation(90);
+            mPreview.refreshCamera(mCamera);
+            Log.d("nu", "null");
+        }else {
+            Log.d("nu","no null");
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //when on Pause, release camera in order to be used from other applications
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        // stop and release camera
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
